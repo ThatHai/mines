bd8eed41b42257edd1dd7488c4ec4f80d4fbabc3
ThatHai <62766020+ThatHai@users.noreply.github.com>
2020-10-20 21:12:23 -0700

    Initial project setup
    Create a gradle project for the application.

---
+++ src/main/java/com/nwsummit/games/mines/Mines.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020 - nwsummit.com
+ *
+ * This software is free under the MIT License (https://www.mit.edu/~amini/LICENSE.md)
+ * The software is provided "as is", without warranty of any kind, expressed or implied.
+ *
+ * Created by: ThatHai on 2020-10-20
+ */
+package com.nwsummit.games.mines;
+
+public class Mines {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Mines().getGreeting());
+    }
+}
