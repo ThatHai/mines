d757e05824be0f6dfa48058fb0fbd1639eef33bd
ThatHai <62766020+ThatHai@users.noreply.github.com>
2020-10-25 00:20:10 -0700

    Refactoring and implementing more features
    * Use NeighbourIterator to visit adjascent cells of a given cell.
    * Move Cell class inside MinesBoard as MinesBoard should be the only
    one able to create Cells.
    * Refactor the construction of MinesBoard objects.
    * Implement flagging a cell/square
    * Add more tests

--- src/main/java/com/nwsummit/games/mines/MinesBoard.java
+++ src/main/java/com/nwsummit/games/mines/MinesBoard.java
@@ -8,14 +8,14 @@
  */
 package com.nwsummit.games.mines;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
 import java.io.PrintStream;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.Random;
 import java.util.Set;
 
@@ -26,110 +26,125 @@ import java.util.Set;
  * <li>the number of mines.</li>
  * </ul>
  */
-class MinesBoard implements Iterable<Cell> {
+class MinesBoard implements Iterable<MinesBoard.Cell> {
+  /**
+   * Value representing a mine.
+   */
+  static final int MINE = -1;
+
   private static final Random RANDOM = new Random(System.currentTimeMillis());
-
-  private static final int MINE = -1;
-
-  private Cell[][] board;
 
   // number of rows and columns
   private final int rows, columns;
 
-  private final Set<Cell> mines = new HashSet<>();
+  private final Cell[][] board;
+
+  private Set<Cell> mines;
+
+  // for testing
+  MinesBoard(int rows, int columns) {
+    checkArgument(rows > 2, "Rows must be greater than 2");
+    checkArgument(columns > 2, "Columns must be greater than 2");
+
+    this.rows = rows;
+    this.columns = columns;
+    this.mines = new HashSet<>();
+    this.board = new Cell[rows][columns];
+    initialize();
+  }
 
   /**
    * Construct a mines board of the specified size, with the specifed number of mines.
+   *
    * @param rows number of rows.
    * @param columns number of columns.
    * @param mines number of mines.
    */
   MinesBoard(int rows, int columns, int mines) {
-    checkArgument(rows > 2, "Rows must be greater than 2");
-    checkArgument(columns > 2, "Columns must be greater than 2");
-    checkArgument(mines > 0 && mines <= (rows * columns / 3),
-                  "There must be at least one, and not too many mines");
-
-    this.rows = rows;
-    this.columns = columns;
-
-    createBoard();
+    this(rows, columns);
     placeMines(mines);
   }
 
   /**
-   * Creates a mines board of the specified size, with the specified mines.
+   * Initialize the whole board with no mine.
    */
-  // for testing
-  MinesBoard(int rows, int columns, Set<Cell> mines) {
-    this.rows = rows;
-    this.columns = columns;
-    this.mines.addAll(mines);
-
-    createBoard();
-    for (Cell mine : mines) {
-      board[mine.row()][mine.col()] = mine;
-      updateMinesCount(mine);
-    }
-  }
-
-  private void createBoard() {
-    board = new Cell[rows][columns];
+  private void initialize() {
     for (int r = 0; r < rows; r++) {
       for (int c = 0; c < columns; c++) {
-        board[r][c] = new Cell(r, c, 0);
+        board[r][c] = new Cell(r, c);
       }
     }
   }
 
-  private void placeMines(int nbMines) {
-    int i = 0;
+  /**
+   * Randomly places the specified number of mines on the board.
+   * @param nMines the number of mines to be placed.
+   */
+  private void placeMines(int nMines) {
+    checkArgument(nMines > 0 && nMines <= (rows * columns), "Invalid number of mines: %d", nMines);
+    mines.clear();
     do {
       // randomly pick a spot on the board
       int r = RANDOM.nextInt(rows);
       int c = RANDOM.nextInt(columns);
-
-      // place a mine there if not already placed
-      if (board[r][c].value() != MINE) {
-	Cell mine = board[r][c] = new Cell(r, c, MINE);
-        mines.add(mine);
-        updateMinesCount(mine);
-        i += 1;
-      }
-    } while (i < nbMines);
+      if (!board[r][c].isMine())
+        placeMine(r, c);
+    } while (mines.size() < nMines);
   }
 
   /**
-   * Given the mine, updates the adjascent mines count of its surrounding cells.
+   * Places a mine at the specified (row, col) and updates the mines count of the cells
+   * adjascent to the mine.
    */
-  private void updateMinesCount(Cell mine) {
-    int row = mine.row();
-    int col = mine.col();
-    for (int r = row-1; r <= row+1; r++) {
-      for (int c = col-1; c <= col+1; c++) {
-        // make sure that (r,c) is within the board and it's not a mine cell
-        if (0 <= r && r < rows && 0 <= c && c < columns &&
-            board[r][c].value() != MINE) {
-          board[r][c].incrementValue(); // increment mines count
-        }
-      }
+  // visible for testing
+  void placeMine(int row, int col) {
+    validate(row, col);
+
+    Cell mine = board[row][col];
+    mine.value = MINE;
+    mines.add(mine);
+
+    // update the mines count of the cells adjascent to the mine
+    Iterator<Cell> neighbours = neighbourIterator(mine);
+    while (neighbours.hasNext()) {
+      Cell adjCell = neighbours.next();
+      if (!adjCell.isMine())
+        adjCell.incrementValue();
     }
   }
 
+  // visible for testing
+  Cell get(int row, int col) {
+    validate(row, col);
+    return board[row][col];
+  }
+
   /**
-   * Opens the specified cell of the mines board.
+   * Flags the specified cell/square of the board as having a mine.
+   */
+  public State flag(int row, int col) {
+    validate(row, col);
+    return board[row][col].flag();
+  }
+
+  /**
+   * Opens the specified cell/square of the mines board.
    *
-   * @return null if the cell is a mine; a non empty collection of opened cells otherwise.
+   * @return null if the cell is a mine;
+             a collection, possibly empty, of opened cells otherwise.
    */
   public List<Cell> open(int row, int col) {
-    checkArgument(0 <= row && row < rows, "Row is out of bound: %d", row);
-    checkArgument(0 <= col && col < columns, "Col is out of bound: %d", col);
+    validate(row, col);
 
     Cell cell = board[row][col];
-    cell.setState(State.OPENED);
+    // nothing to open if already opened of flagged
+    if (cell.state == State.OPENED || cell.state == State.FLAGGED) {
+      return Collections.emptyList();
+    }
 
-    // opened cell is a mine
-    if (cell.value() == MINE) {
+    cell.state = State.OPENED;
+
+    if (cell.isMine()) {
       return null;
     }
 
@@ -142,8 +157,8 @@ class MinesBoard implements Iterable<Cell> {
     // if the opened cell is not adjascent to any mines, then open its neighbouring cells
     //
     // cells opening can be seen as a breadth frist search in a graph, with cells being vertices.
-    LinkedList<Cell> opened = new LinkedList<>();
-    opened.add(cell);
+    LinkedList<Cell> openedCells = new LinkedList<>();
+    openedCells.add(cell);
 
     // queue is used to store the (adjascent) cells to work on at next iteration
     LinkedList<Cell> queue = new LinkedList<>();
@@ -152,34 +167,47 @@ class MinesBoard implements Iterable<Cell> {
     while (!queue.isEmpty()) {
       cell = queue.poll();
       if (cell.value() == 0) {
-        int cr = cell.row();
-        int cc = cell.col();
-        // iterate the cell's adjascent cells and open them
-        for (int r = cr-1; r <= cr+1; r++) {
-          for (int c = cc-1; c <= cc+1; c++) {
-            if (0 <= r && r < rows && 0 <= c && c < columns) {
-              Cell adjCell = board[r][c];
-              // add to the queue if it hasn't been previously opened
-              if (adjCell.state() == State.UNOPENED) {
-                opened.add(adjCell);
-                queue.add(adjCell);
-                adjCell.setState(State.OPENED);
-              }
-            }
+        Iterator<Cell> neighbours = neighbourIterator(cell);
+        while (neighbours.hasNext()) {
+          Cell neighbour = neighbours.next();
+          if (neighbour.state == State.UNOPENED) {
+            neighbour.state = State.OPENED;
+            openedCells.add(neighbour);
+            queue.add(neighbour); // to work on at next open iteration
           }
         }
       }
     }
-    return opened;
+    return openedCells;
+  }
+
+  private void checkArgument(boolean condition, String message, Object ... args) {
+    if (!condition) {
+      throw new IllegalArgumentException(String.format(message, args));
+    }
+  }
+
+  private void validate(int row, int col) throws IllegalArgumentException {
+    checkArgument(0 <= row && row < rows, "Row out of bound: %d", row);
+    checkArgument(0 <= col && col < columns, "Col out of bound: %d", col);
   }
 
   @Override
-  public java.util.Iterator<Cell> iterator() {
-    return new Iterator();
+  public Iterator<Cell> iterator() {
+    return new BoardIterator();
+  }
+
+  Iterator<Cell> neighbourIterator(int row, int col) {
+    validate(row, col);
+    return neighbourIterator(board[row][col]);
+  }
+
+  private Iterator<Cell> neighbourIterator(Cell cell) {
+    return new NeighbourIterator(cell);
   }
 
   /**
-   * Returns this board's collection of mines.
+   * Returns the mines on this board.
    */
   Set<Cell> getMines() {
     return Collections.unmodifiableSet(mines);
@@ -188,7 +216,7 @@ class MinesBoard implements Iterable<Cell> {
   void print(PrintStream stream) {
     for (int r = 0; r < rows; r++) {
       for (int c = 0; c < columns; c++) {
-        Object obj = board[r][c].value() == MINE ? "M" : board[r][c].value();
+        Object obj = board[r][c].isMine() ? "M" : board[r][c].value();
         stream.printf(" %s", obj);
       }
       stream.println();
@@ -199,10 +227,10 @@ class MinesBoard implements Iterable<Cell> {
    * Class for iterating through the {@link MinesBoard} row by row. i.e. it iterates through the
    * cells of the first row, then of the second row, etc.
    */
-  class Iterator implements java.util.Iterator<Cell> {
+  class BoardIterator implements Iterator<Cell> {
     private int curRow, curCol;
 
-    private Iterator() {
+    private BoardIterator() {
       curRow = curCol = 0;
     }
 
@@ -227,6 +255,115 @@ class MinesBoard implements Iterable<Cell> {
         }
       }
       return cell;
+    }
+  }
+
+  class NeighbourIterator implements Iterator<Cell> {
+    private Cell[] neighbours;
+    private int i = 0;
+
+    NeighbourIterator(Cell cell) {
+      LinkedList<Cell> neighbourList = new LinkedList<>();
+      int rmax = Math.min(cell.row() + 1, MinesBoard.this.rows -1);
+      int cmax = Math.min(cell.col() + 1, MinesBoard.this.columns -1);
+      for (int r = Math.max(cell.row() - 1, 0); r <= rmax; r++) {
+        for (int c = Math.max(cell.col() - 1, 0); c <= cmax; c++) {
+          neighbourList.add(board[r][c]);
+        }
+      }
+      neighbourList.remove(cell);
+      neighbours = neighbourList.toArray(new Cell[neighbourList.size()]);
+    }
+
+    @Override
+    public boolean hasNext() {
+      return i < neighbours.length;
+    }
+
+    @Override
+    public Cell next() {
+      if (i >= neighbours.length) {
+        throw new NoSuchElementException();
+      }
+      return neighbours[i++];
+    }
+
+    int size() {
+      return neighbours.length;
+    }
+  }
+
+  class Cell {
+    private final int row;
+    private final int col;
+    private int value;
+    private State state;
+
+    Cell(int row, int col) {
+      this.row = row;
+      this.col = col;
+      this.value = 0;
+      this.state = State.UNOPENED;
+    }
+
+    /**
+     * The mines board's row (zero-base) where this cell is.
+     */
+    int row() {
+      return row;
+    }
+
+    /**
+     * The mines board's column (zero-base) where this cell is.
+     */
+    int col() {
+      return col;
+    }
+
+    int value() {
+      return value;
+    }
+
+    private void incrementValue() {
+      value += 1;
+    }
+
+    boolean isMine() {
+      return value == MINE;
+    }
+
+    State state() {
+      return state;
+    }
+
+    State flag() {
+      if (state == State.OPENED)
+        return State.OPENED;
+      state = (state == State.UNOPENED) ? State.FLAGGED : State.UNOPENED;
+      return state;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(row, col, value);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj)
+        return true;
+      if (obj == null || getClass() != obj.getClass())
+        return false;
+
+      Cell that = (Cell) obj;
+      return this.row == that.row
+        && this.col == that.col
+        && this.value == that.value;
+    }
+
+    @Override
+    public String toString() {
+      return "Cell[" + row + "," + col + "," + value + "]";
     }
   }
 }
--- src/test/java/com/nwsummit/games/mines/MinesBoardTest.java
+++ src/test/java/com/nwsummit/games/mines/MinesBoardTest.java
@@ -8,17 +8,20 @@
  */
 package com.nwsummit.games.mines;
 
+import static org.junit.Assert.assertTrue;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotEquals;
 import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertSame;
 
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
+import com.google.common.collect.Sets;
+import com.nwsummit.games.mines.MinesBoard.NeighbourIterator;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -45,19 +48,58 @@ public class MinesBoardTest {
   public void testBoardRandomness() {
     MinesBoard board1 = new MinesBoard(4, 4, 3);
     board1.print(System.out);
-    Set<Cell> mines1 = board1.getMines();
+    Set<MinesBoard.Cell> mines1 = board1.getMines();
 
     System.out.println();
 
     MinesBoard board2 = new MinesBoard(4, 4, 3);
     board2.print(System.out);
-    Set<Cell> mines2 = board2.getMines();
+    Set<MinesBoard.Cell> mines2 = board2.getMines();
 
     // if the placement of mines is really random, then two board of the same size and
     // same mines number would not have the same location for the mines.
     assertEquals(mines1.size(), 3, "Number of mines, board1");
     assertEquals(mines2.size(), 3, "Number of mines, board2");
     assertNotEquals(mines1, mines2, "Mines from board1 and board2");
+  }
+
+  @Test
+  public void testNeighbourIterator_CornerCells() {
+    MinesBoard board = new MinesBoard(4, 4, 1);
+    NeighbourIterator neighbours = (NeighbourIterator)board.neighbourIterator(0, 0);
+    assertEquals(neighbours.size(), 3);
+
+    neighbours = (NeighbourIterator)board.neighbourIterator(3, 0);
+    assertEquals(neighbours.size(), 3);
+
+    neighbours = (NeighbourIterator)board.neighbourIterator(0, 3);
+    assertEquals(neighbours.size(), 3);
+
+    neighbours = (NeighbourIterator)board.neighbourIterator(3, 3);
+    assertEquals(neighbours.size(), 3);
+  }
+
+  @Test
+  public void testNeighbourIterator_EdgeCells() {
+    MinesBoard board = new MinesBoard(4, 4, 1);
+    NeighbourIterator neighbours = (NeighbourIterator)board.neighbourIterator(0, 2);
+    assertEquals(neighbours.size(), 5);
+
+    neighbours = (NeighbourIterator)board.neighbourIterator(1, 0);
+    assertEquals(neighbours.size(), 5);
+
+    neighbours = (NeighbourIterator)board.neighbourIterator(3, 2);
+    assertEquals(neighbours.size(), 5);
+
+    neighbours = (NeighbourIterator)board.neighbourIterator(2, 3);
+    assertEquals(neighbours.size(), 5);
+  }
+
+  @Test
+  public void testNeighbourIterator_InsideCells() {
+    MinesBoard board = new MinesBoard(4, 4, 1);
+    NeighbourIterator neighbours = (NeighbourIterator)board.neighbourIterator(1, 2);
+    assertEquals(neighbours.size(), 8);
   }
 
   @Test
@@ -69,64 +111,29 @@ public class MinesBoardTest {
     MinesBoard board = new MinesBoard(rows, cols, rows * cols / 4);
     board.print(System.out);
 
-    // our working board
-    int[][] expected = new int[rows][cols];
+    int cellCount = 0;
+    Iterator<MinesBoard.Cell> boardIterator = board.iterator();
+    while (boardIterator.hasNext()) {
+      MinesBoard.Cell cell = boardIterator.next();
+      cellCount += 1;
+      if (cell.isMine())
+        continue;
 
-    // place the mines at the same locations in our board
-    for (Cell mine: board.getMines()) {
-      expected[mine.row()][mine.col()] = -1;
-    }
-
-    // with the mines in place, we'll just use the brute force method to compute the
-    // number of adjascent mines for all cells of the board. (can't use the same algo
-    // as in MinesBoard since we want to test it)
-    for (int r = 0; r < rows; r++) {
-      for (int c = 0; c < cols; c++) {
-        // cell is a mine => no counting
-        if (expected[r][c] == -1) {
-          continue;
+      int mineCount = 0;
+      Iterator<MinesBoard.Cell> neighbourIterator = board.neighbourIterator(cell.row(), cell.col());
+      while (neighbourIterator.hasNext()) {
+        MinesBoard.Cell neighbour = neighbourIterator.next();
+        if (neighbour.isMine()) {
+          mineCount += 1;
         }
-
-        // iterate through the adjascent cell and count the number of mines
-        int count = 0;
-        for (int i = r-1; i <= r+1; i++) {
-          for (int j = c-1; j <= c+1; j++) {
-            if (i < 0 || i == rows ||
-                j < 0 || j == cols ||
-                (i == r && j == c)) {
-              continue;
-            }
-            if (expected[i][j] == -1) {
-              count += 1;
-            }
-          }
-        }
-        expected[r][c] = count;
       }
+      assertEquals(cell.value(), mineCount, "Mines count for cell " + cell);
     }
-
-    Iterator<Cell> iterator = board.iterator();
-    while (iterator.hasNext()) {
-      Cell cell = iterator.next();
-      assertEquals(cell.value(), expected[cell.row()][cell.col()],
-                   "Cell (" + cell.row() + "," + cell.col() + ")");
-    }
+    assertEquals(cellCount, rows * cols, "Number of iterated cells");
   }
 
   @Test
-  public void testOpen() {
-    HashSet<Cell> mines = new HashSet<>();
-    mines.add(new Cell(0, 3, -1));
-    mines.add(new Cell(0, 5, -1));
-    mines.add(new Cell(1, 6, -1));
-    mines.add(new Cell(2, 3, -1));
-    mines.add(new Cell(2, 6, -1));
-    mines.add(new Cell(5, 0, -1));
-    mines.add(new Cell(6, 0, -1));
-    mines.add(new Cell(6, 1, -1));
-    mines.add(new Cell(6, 5, -1));
-    mines.add(new Cell(7, 7, -1));
-
+  public void testOpen_SomeAlreadyOpened() {
     /*
       The testing mines board looks like the below:
       (0,0)        columns
@@ -140,22 +147,115 @@ public class MinesBoardTest {
       s |  M M 1 0 1 M 2 1
         v  2 2 1 0 1 1 2 M
     */
-    MinesBoard board = new MinesBoard(8, 8, mines);
+    MinesBoard board = new MinesBoard(8, 8);
+    board.placeMine(0, 3);
+    board.placeMine(0, 5);
+    board.placeMine(1, 6);
+    board.placeMine(2, 3);
+    board.placeMine(2, 6);
+    board.placeMine(5, 0);
+    board.placeMine(6, 0);
+    board.placeMine(6, 1);
+    board.placeMine(6, 5);
+    board.placeMine(7, 7);
+
     board.print(System.out);
     System.out.println();
 
-    assertNull(board.open(6, 0), "Open a mine");
-    assertNull(board.open(1, 6), "Open a mine");
+    assertEquals(board.open(7, 0), Collections.singleton(board.get(7, 0)));
+    assertEquals(board.open(5, 1), Collections.singleton(board.get(5, 1)));
+    assertEquals(board.open(5, 6), Collections.singleton(board.get(5, 6)));
 
-    assertEquals(board.open(7, 0), Collections.singletonList(new Cell(7, 0, 2)),
-                 "Open cell with 2 adjascent mines");
-    assertEquals(board.open(5, 1), Collections.singletonList(new Cell(5, 1, 3)),
-                 "Open cell with 3 adjascent mines");
-    assertEquals(board.open(5, 6), Collections.singletonList(new Cell(5, 6, 1)),
-                 "Open cell with 1 adjascent mine");
-
-    List<Cell> opened = board.open(4, 2);
+    List<MinesBoard.Cell> opened = board.open(4, 2);
     // since (5,1) and (5,6) are already opened above, we get 38 instead of 40
     assertEquals(opened.size(), 38, "Opened");
+  }
+
+  /*
+    This testing mines board looks like the below:
+    (0,0)        columns
+      +----------------->
+      |  0 1 M 1 0 1 2 2
+      |  1 2 1 1 0 1 M M
+      |  M 2 1 1 0 1 3 M
+    r |  1 2 M 1 0 0 1 1
+    o |  0 2 2 3 1 1 0 0
+    w |  1 2 M 2 M 1 0 0
+    s |  M 2 1 2 2 2 1 0
+      v  1 1 0 0 1 M 1 0
+  */
+  private MinesBoard testingBoard() {
+    MinesBoard board = new MinesBoard(8, 8);
+    board.placeMine(0, 2);
+    board.placeMine(1, 6);
+    board.placeMine(1, 7);
+    board.placeMine(2, 0);
+    board.placeMine(2, 7);
+    board.placeMine(3, 2);
+    board.placeMine(5, 2);
+    board.placeMine(5, 4);
+    board.placeMine(6, 0);
+    board.placeMine(7, 5);
+    return board;
+  }
+
+  @Test
+  public void testOpen_Mine() {
+    MinesBoard board = testingBoard();
+    assertNull(board.open(0, 2));
+    assertNull(board.open(2, 0));
+    assertNull(board.open(7, 5));
+    assertNull(board.open(1, 7));
+    assertNull(board.open(3, 2));
+  }
+
+  @Test
+  public void testOpen_SomeFlagged() {
+    MinesBoard board = testingBoard();
+    List<MinesBoard.Cell> opened = board.open(0, 4);
+    assertEquals(opened.size(), 28);
+
+    // same tesing board
+    board = testingBoard();
+    // this is a square with no adjascent mines
+    assertSame(board.flag(3, 5), State.FLAGGED);
+
+    opened = board.open(0, 4);
+    assertEquals(opened.size(), 14);
+  }
+
+  @Test
+  public void testOpen_EmptyCellUnconnected() {
+    MinesBoard board = testingBoard();
+    assertEquals(Sets.newHashSet(board.open(0, 0)),
+                 Sets.newHashSet(board.get(0, 0),
+                                 board.get(0, 1),
+                                 board.get(1, 0),
+                                 board.get(1, 1)));
+  }
+
+  @Test
+  public void testOpen_AlreadyOpenedCell() {
+    MinesBoard board = new MinesBoard(3, 3);
+    board.open(0, 0);
+
+    assertTrue(board.open(0, 0).isEmpty());
+  }
+
+  @Test
+  public void testOpen_FlaggedCell() {
+    MinesBoard board = new MinesBoard(3, 3);
+    assertEquals(board.flag(0, 0), State.FLAGGED);
+    assertTrue(board.open(0, 0).isEmpty());
+  }
+
+  @Test
+  public void testFlag() {
+    MinesBoard board = new MinesBoard(3, 3);
+    assertEquals(board.flag(0, 0), State.FLAGGED);
+    assertEquals(board.flag(0, 0), State.UNOPENED);
+
+    board.open(0, 0);
+    assertEquals(board.flag(0, 0), State.OPENED);
   }
 }
